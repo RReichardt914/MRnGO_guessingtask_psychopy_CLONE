817c62d519a4dbc62a5568b85f38109b0ddaddb1
Richárd Reichardt <reichardt.richard@ppk.elte.hu>
2026-03-02 18:42:38 +0100

    welcome - 8

--- untitled-legacy-browsers.js
+++ untitled-legacy-browsers.js
@@ -177,7 +177,7 @@ async function experimentInit() {
     ori : 0.0, 
     pos : [0, 0], 
     draggable: false,
-    size : 1.0,
+    size : [268.5, 87],
     color : new util.Color([1,1,1]), opacity : undefined,
     flipHoriz : false, flipVert : false,
     texRes : 128.0, interpolate : true, depth : -1.0 
@@ -483,8 +483,6 @@ function start_screenRoutineEachFrame() {
     
     // *btn_start_img* updates
     if (t >= 0.0 && btn_start_img.status === PsychoJS.Status.NOT_STARTED) {
-      // update params
-      btn_start_img.setSize([0.5, 0.5], false);
       // keep track of start time/frame for later
       btn_start_img.tStart = t;  // (not accounting for frame time here)
       btn_start_img.frameNStart = frameN;  // exact frame index
@@ -495,8 +493,6 @@ function start_screenRoutineEachFrame() {
     
     // if btn_start_img is active this frame...
     if (btn_start_img.status === PsychoJS.Status.STARTED) {
-      // update params
-      btn_start_img.setSize([0.5, 0.5], false);
     }
     
     // *click_start_mouse* updates
