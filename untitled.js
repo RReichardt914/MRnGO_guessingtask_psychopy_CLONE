3bf27c8e6e6dc541e59fe3d2a0401284967403d6
Richárd Reichardt <reichardt.richard@ppk.elte.hu>
2026-03-04 12:33:24 +0100

    feedback - v11

--- untitled.js
+++ untitled.js
@@ -608,7 +608,7 @@ async function experimentInit() {
     units: 'norm', 
     pos: [(- 0.5), 0.4], draggable: false, height: 0.05,  wrapWidth: undefined, ori: 0.0,
     languageStyle: 'LTR',
-    color: new util.Color((-1.0000, -1.0000, -1.0000)),  opacity: undefined,
+    color: new util.Color((-1.0000, -1.0000, 0.0902)),  opacity: undefined,
     depth: -4.0 
   });
   
