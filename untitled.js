998f51b67fe55da1ef7a8e3a880faae08ceae3aa
Richárd Reichardt <reichardt.richard@ppk.elte.hu>
2026-03-04 12:35:04 +0100

    v12

--- untitled.js
+++ untitled.js
@@ -606,7 +606,7 @@ async function experimentInit() {
     text: '',
     font: 'Arial',
     units: 'norm', 
-    pos: [(- 0.5), 0.4], draggable: false, height: 0.05,  wrapWidth: undefined, ori: 0.0,
+    pos: [(- 0.5), 0.3], draggable: false, height: 0.1,  wrapWidth: undefined, ori: 0.0,
     languageStyle: 'LTR',
     color: new util.Color((-1.0000, -1.0000, 0.0902)),  opacity: undefined,
     depth: -4.0 
